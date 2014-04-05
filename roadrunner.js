a225de4520215267b289392c020b8017104fa438
jeanturban <jeanalex23@gmail.com>
2014-04-05 02:07:46 -0700

    uses current area

--- roadrunner.js
+++ roadrunner.js
@@ -9,7 +9,6 @@ var elevations = [];
 function initialize() {
   var mapOptions = {
     zoom: 8,
-    center: area,
     mapTypeId: 'terrain'
   }
   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
@@ -23,6 +22,40 @@ function initialize() {
 
   // Add a listener for the click event and call getElevation on that location
   google.maps.event.addListener(map, 'click', getElevation);
+ // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
+
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+
+}
+
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
 }
 
 function calcRoute() {
